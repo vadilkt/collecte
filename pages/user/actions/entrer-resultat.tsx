3e844cce9873d3e5aca6f3740981cfa912c257f1
nguekeu <nguekeu3divin@gmail.com>
2022-08-22 11:31:49 +0100

    Ajout du champ moyens pour l'enregistrement des resultats pour un objectif

--- pages/user/actions/entrer-resultat.tsx
+++ pages/user/actions/entrer-resultat.tsx
@@ -1,6 +1,25 @@
 import Head from "next/head"
 import Dashboard from "../../../components/dashboard";
-import {TextInput, Button, Label} from "flowbite-react";
+import {TextInput, Button, Label, Checkbox} from "flowbite-react";
+
+
+const moyens : {name: string}[] = [
+  {
+    name: "Téléphone",
+  },
+  {
+    name: "Internet",
+  },
+  {
+    name: "Diners",
+  },
+  {
+    name: "Apéritifs",
+  },
+  {
+    name: "Dotation carburant",
+  }
+]
 
 
 export default () => {
@@ -17,6 +36,7 @@ export default () => {
         <h3 className="text-xl mt-4 text-gray-700"> Entrer les resultats </h3>
         <div className="mt-4 bg-white p-8 rounded-lg">
             <form className="flex flex-col gap-4">
+
             <div>
               <div className="mb-2 block">
                 <Label
@@ -31,6 +51,30 @@ export default () => {
                 disabled={true}
               />
             </div>
+
+
+            <div className="">
+                <div className="mb-2 block">
+                  <Label
+                    value="Moyens"
+                  />
+                </div>
+                  {
+                    moyens.map( item => 
+                      <div
+                        className="flex flex-col gap-4"
+                        id="checkbox"
+                      >
+                        <div className="flex items-center gap-2">
+                          <Checkbox
+                            id="accept"
+                          />
+                          <div>{item.name}</div>
+                        </div>
+                      </div>
+                    )
+                  }
+              </div>
 
             <div>
               <div className="mb-2 block">
